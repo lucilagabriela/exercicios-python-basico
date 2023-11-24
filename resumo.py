73ba147128f88abbb87e9bbafcb17f2734bc96fb
Lucila Costa <lucilagdac@gmail.com>
2023-11-24 10:23:40 -0300

    Update resumo.py

--- resumo.py
+++ resumo.py
@@ -1,4 +1,24 @@
-x = 3
+""" comentario: interpretador ignora
+ DocString: nao eh comentario. o interpretador do python le
+ linguagem case sensitive: sensivel a letras maiusculas e minusculas
+ tipo de tipagem: dinamica / forte 
+ tipagem dinamica: sabe o tipo de informacao q esta colocando
+ tipagem forte: 
+"""
+
+
+# entrada de dados
+#numero = int(input("Digite um numero: "))
 
 # saida de dados
-print(x)
+#print(numero) # numero eh argumento (esta dentro do parenteses)
+print(18, 6, sep="-", end='\n\r') # separar os arumentos
+print("hello world")
+
+"""
+tipos de dados/variaveis:
+- Strings Str: textos que estao dentro de aspas simples ou duplas
+- int:
+- float: ponto flueante
+
+"""
